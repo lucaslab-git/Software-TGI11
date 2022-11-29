7331aa4bf7db2f3304ff82a10c6c28a5c737653e
Luca's Lab <92208704+lucaslab-git@users.noreply.github.com>
2022-11-29 09:43:48 +0100

    Add files via upload

---
+++ Volljährigskeitskontrolle/Volljährigskeitkontrolle/Volljährigskeitkontrolle/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Volljährigskeitkontrolle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Programm: Volljährigkeitskontrolle";
+            Console.WriteLine("\x1B[4mProgramm: Volljährigkeitskontrolle\x1B[0m");
+            Console.WriteLine("");
+            Console.Write("Geben sie ihr Alter in einer Ganzahl ein: ");
+
+            if (Convert.ToInt16(Console.ReadLine()) >= 18){
+                Console.WriteLine("Sie sind Volljährig");
+            }else{
+                Console.WriteLine("Sie sind nicht Volljährig");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("Bitte Enter zum Schließen drücken!");
+
+            Console.ReadKey();
+        }
+    }
+}
