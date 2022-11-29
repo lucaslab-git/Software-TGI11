bc50c3477920575c7c7c87512f3bb05638bc9055
Luca's Lab <92208704+lucaslab-git@users.noreply.github.com>
2022-11-29 09:48:00 +0100

    Add files via upload

---
+++ Summen-Berechnung/Rätselhaftes Program/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Rätselhaftes_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int zahl1;
+            int zahl2;
+            int summe;
+
+            Console.Title = "Rätselhaftes Program";
+
+            Console.WriteLine("\x1B[4mRätselhaftes Programm: Zahlen addieren\x1B[0m");
+            Console.WriteLine("");
+            Console.Write("Bitte geben Sie die 1.Zahl und bestätigen sie Enter: ");
+            zahl1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Bitte geben Sie die 2.Zahl und bestätigen sie Enter: ");
+            zahl2 = Convert.ToInt32(Console.ReadLine());
+
+            summe = zahl1 + zahl2;
+
+            Console.WriteLine("Das Ergebnis ist: " + summe);
+
+            Console.WriteLine("");
+            Console.WriteLine("Bitte Enter zum Schließen drücken!");
+
+            Console.ReadKey();
+
+        }
+    }
+}
